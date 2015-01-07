536023454af2936a82df2a0f85d59bd96bde62b3
Tom Gale <tom@tg.id.au>
2015-01-07 18:13:18 +1100

    Retrieve mail contents

--- imap-pushover2.js
+++ imap-pushover2.js
@@ -15,9 +15,8 @@ if(process.argv[2]) {
 
 try {
   config = yaml.safeLoad(fs.readFileSync(config_file, 'utf8'));
-} catch(e) {
-  console.log(e);
-  process.exit(1);
+} catch(err) {
+  errorExit(err);
 }
 
 dbg('configuration: %s', JSON.stringify(config));
@@ -33,31 +32,70 @@ var imap_config = {
 var conn = new imap(imap_config);
 var seen_msgs = []; //UIDs of seen messages
 
-function mainReady() {
+function connReady() {
   dbg('connection ready');
-  mainInbox();
-}
-
-function mainInbox() {
-  dbg('opening inbox');
-  conn.openBox(config.mailbox, true, function(err, box) {
+  dbg('opening imap folder %s', config.folder);
+  conn.openBox(config.folder, true, function(err, box) {
     if(err) {
-      console.log(err);
-      conn.end();
+      errorExit(err)
     }
-    dbg('inbox opened');
+    dbg('%s opened', config.folder);
   });
 }
 
-function mainMailArrived(count) {
-  dbg('%s new messages found', count);
+function connMail(count) {
+  dbg('%s new messages arrived', count);
+  conn.search(['UNSEEN'], function(err, results) {
+    dbg('searching for new unread messages');
+    if(err) {
+      errorExit(err);
+    }
+    dbg('found unread messages %s', JSON.stringify(results));
+    dbg('seen messages %s', JSON.stringify(seen_msgs));
+    for (var i = results.length - 1; i >= 0; i--) {
+      if(seen_msgs.indexOf(results[i]) == -1) {
+        dbg('going to retrieve %s', results[i]);
+        connRetrieve(results[i]);
+      } else {
+        dbg('skipping %s', results[i]);
+      }
+    };
+  });
 }
 
-function mainExpunge(seqno) {
+function connRetrieve(uid) {
+  var f = conn.fetch([uid], { bodies: ['HEADER.FIELDS (TO FROM CC SUBJECT)', 'TEXT'] });
+  f.on('message', function(msg, seqno) {
+    dbg('retrieved message %s as %s', uid, seqno);
+     msg.on('body', function(stream, info) {
+      var buffer = '';
+      stream.on('data', function(chunk) {
+        buffer += chunk.toString('utf8');
+      });
+      stream.once('end', function() {
+        dbg('end of message part stream');
+        if (info.which !== 'TEXT') {
+          dbg('Parsed header: %s', util.inspect(imap.parseHeader(buffer)));
+        } else {
+          dbg(buffer);
+        }
+      });
+    });
+    msg.once('end', function() {
+      dbg('end of message parts');
+    });
+  });
+  f.once('error', errorExit);
+  f.once('end', function() {
+    dbg('finished retrieving %s', uid);
+  });
+}
+
+function connExpunge(seqno) {
   dbg('message sequence number %s was expunged', seqno);
 }
 
-function mainClose(e_bool) {
+function connClose(e_bool) {
   if(e_bool) {
     dbg('connection closed with error');
   } else {
@@ -65,18 +103,20 @@ function mainClose(e_bool) {
   }
 }
 
-function mainError(err) {
+function errorExit(err) {
   console.log(err);
+  process.exit(1);
 }
 
-function mainEnd() {
+function connEnd() {
   console.log('connection ended');
 }
 
-conn.on('ready', mainReady);
-conn.on('mail', mainMailArrived);
-conn.on('expunge', mainExpunge);
-conn.on('close', mainClose);
-conn.on('error', mainError);
-conn.on('end', mainEnd);
+conn.on('ready', connReady);
+conn.on('mail', connMail);
+conn.on('expunge', connExpunge);
+conn.on('close', connClose);
+conn.on('error', errorExit);
+conn.on('end', connEnd);
+dbg('opening connection');
 conn.connect();
